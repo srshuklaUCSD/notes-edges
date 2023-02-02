770ff5b32857cfb662deffc7db8eec820393834a
srshuklaUCSD <77903091+srshuklaUCSD@users.noreply.github.com>
2023-02-02 00:31:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,36 @@
-// Define the data for two nodes
-var nodes = [
-  { name: "One" },
-  { name: "Two" }
-];
+const width = 500;
+const height = 500;
 
-// Define the links between the nodes
-var links = [
-  { source: nodes[0], target: nodes[1] }
-];
-
-// Set the size of the graph
-var width = 500, height = 500;
-
-// Create the svg canvas
-var svg = d3.select("body")
+const svg = d3.select("body")
   .append("svg")
   .attr("width", width)
   .attr("height", height);
 
-// Create the force simulation
-var simulation = d3.forceSimulation(nodes)
-  .force("link", d3.forceLink(links).id(function(d) { return d.name; }))
-  .force("charge", d3.forceManyBody())
-  .force("center", d3.forceCenter(width / 2, height / 2));
+const nodes = [
+  {id: "one", x: 100, y: 200},
+  {id: "two", x: 400, y: 100}
+];
 
-// Add the links to the svg
-var link = svg.append("g")
-  .attr("class", "links")
-  .selectAll("line")
-  .data(links)
-  .enter().append("line")
+const edges = [
+  {source: "one", target: "two"}
+];
+
+const link = svg.selectAll(".link")
+  .data(edges)
+  .enter()
+  .append("line")
+  .attr("x1", d => {return d.source.x})
+  .attr("y1", d => {return d.source.y})
+  .attr("x2", d => {return d.target.x})
+  .attr("y2", d => {return d.target.y})
+  .attr("stroke", "black")
   .attr("stroke-width", 2);
 
-// Add the nodes to the svg
-var node = svg.append("g")
-  .attr("class", "nodes")
-  .selectAll("circle")
+const node = svg.selectAll(".node")
   .data(nodes)
-  .enter().append("circle")
+  .enter()
+  .append("circle")
+  .attr("cx", d => {return d.x})
+  .attr("cy", d => {return d.y})
   .attr("r", 10)
-  .attr("fill", "lightblue");
-
-// Add the node names to the svg
-var label = svg.append("g")
-  .attr("class", "labels")
-  .selectAll("text")
-  .data(nodes)
-  .enter().append("text")
-  .text(function(d) { return d.name; });
-
-// Update the graph on each tick of the simulation
-simulation.on("tick", function() {
-  link.attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
-  node.attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
-  label.attr("x", function(d) { return d.x + 15; })
-    .attr("y", function(d) { return d.y; });
-});
+  .attr("fill", "red");
